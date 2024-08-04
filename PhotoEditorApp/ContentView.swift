43b5dbb2646cdc634d0dcd4b5f8dcd1855d83d43
asadbek824 <yoldoshev5007@icloud.com>
2024-08-04 22:03:16 +0500

    Initial Commit

---
+++ PhotoEditorApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoEditorApp
+//
+//  Created by Asadbek Yoldoshev on 04/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoEditorApp/PhotoEditorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorAppApp.swift
+//  PhotoEditorApp
+//
+//  Created by Asadbek Yoldoshev on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
